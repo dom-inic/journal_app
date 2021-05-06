fbda3fa3ffdd29f4899a2f3fcea35d5e73c27a69
dom-inic <dominicnyambane22@gmail.com>
2021-05-06 20:59:36 +0300

    searchJournal widget

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/main.dart';
+import '../main.dart';
 
 class AboutPage extends StatelessWidget {
+
+  handlesubmit() {
+    final greetings = "hello, welcome to my journal app";
+    return greetings;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('About US'),),
+      appBar: AppBar(
+        title: Text('About US'),
+      ),
       body: Column(
         children: [
-          
-          Text('journal app is an app that will change the way you view your life. its an absolute game changer. imagine keeping your journals in your phone since our world revolves around smart phones nowadays')
+          searchJournal(),
+          Text(
+              'journal app is an app that will change the way you view your life. its an absolute game changer. imagine keeping your journals in your phone since our world revolves around smart phones nowadays'),
+          FlatButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()));
+              },
+              child: Text('click me')),
         ],
-
-
       ),
-      
-      
-      
+    );
+  }
+
+  Widget searchJournal(){
+    return Column(
+      children: [
+        Text("search"),
+        Icon(Icons.search),
+      ],
     );
   }
 }
